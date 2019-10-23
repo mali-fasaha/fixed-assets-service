c67f8c912c3e984871a018ce128dc7b41ccfacf6
Edwin Njeru <mailnjeru@gmail.com>
2019-10-23 14:26:47 +0300

    Add queued-resource interface

--- src/main/java/io/github/assets/app/resource/QueuedResource.java
+++ src/main/java/io/github/assets/app/resource/QueuedResource.java
@@ -7,11 +7,30 @@ import java.net.URISyntaxException;
 
 public interface QueuedResource<DTO> {
 
+    /**
+     * Create entity response
+     *
+     * @param requestDTO
+     * @return
+     * @throws URISyntaxException
+     */
     ResponseEntity<DTO> createEntity(DTO requestDTO) throws URISyntaxException;
 
 
+    /**
+     * Update entity sent in the request
+     *
+     * @param requestDTO
+     * @return
+     * @throws URISyntaxException
+     */
     ResponseEntity<FileUploadDTO> updateEntity(DTO requestDTO) throws URISyntaxException;
 
-
+    /**
+     * Delete the entity whose ID
+     *
+     * @param id
+     * @return
+     */
     ResponseEntity<Void> deleteEntity(Long id);
 }
