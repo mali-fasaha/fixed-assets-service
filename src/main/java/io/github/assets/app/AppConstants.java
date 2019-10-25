d104f3f51d13dc4bbe41a2248aa7fac43c4cce25
Edwin Njeru <mailnjeru@gmail.com>
2019-10-25 07:56:44 +0300

    Add general date-time-formatter

--- src/main/java/io/github/assets/app/AppConstants.java
+++ src/main/java/io/github/assets/app/AppConstants.java
@@ -1,11 +1,15 @@
 package io.github.assets.app;
 
+import java.time.format.DateTimeFormatter;
+
 public class AppConstants {
 
     // Regex for acceptable logins
     public static final String EMAIL_REGEX = "^[_.@A-Za-z0-9-]*$";
 
     public static final int TOKEN_BYTE_LENGTH = 20;
+    public static final String DATETIME_FORMAT = "yyyy/MM/dd";
+    public static DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern(DATETIME_FORMAT);
 
     private AppConstants() {
     }
