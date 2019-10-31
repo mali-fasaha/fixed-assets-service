21ebcb24abdad7ff08cdd433ff6329da27ef2524
Edwin Njeru <mailnjeru@gmail.com>
2019-10-31 15:04:07 +0300

    Remove unused components from the sample streams tests

--- src/test/java/io/github/assets/app/messaging/platform/GreetingsControllerIT.java
+++ src/test/java/io/github/assets/app/messaging/platform/GreetingsControllerIT.java
@@ -4,26 +4,21 @@ import io.github.assets.FixedAssetServiceApp;
 import io.github.assets.app.messaging.MessageService;
 import io.github.assets.app.messaging.TokenizableMessage;
 import io.github.assets.app.messaging.sample.Greetings;
-import io.github.assets.app.messaging.sample.GreetingsListener;
 import io.github.assets.app.messaging.sample.GreetingsStreams;
 import io.github.assets.app.util.TokenGenerator;
 import io.github.assets.config.SecurityBeanOverrideConfiguration;
 import io.github.assets.domain.MessageToken;
-import io.github.assets.web.rest.errors.ExceptionTranslator;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.SerializationUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.test.binder.MessageCollector;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.util.MimeTypeUtils;
-import org.springframework.validation.Validator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,19 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GreetingsControllerIT {
 
     @Autowired
-    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
-
-    @Autowired
-    private ExceptionTranslator exceptionTranslator;
-
-    @Autowired
-    private Validator validator;
-
-    @Autowired
     private MessageService<TokenizableMessage<String>> greetingsService;
-
-    @Mock
-    private GreetingsListener greetingsListener;
 
     @Autowired
     private MessageCollector messageCollector;
@@ -87,11 +70,7 @@ public class GreetingsControllerIT {
         String description = "About the Starks";
         long timestamp = System.currentTimeMillis();
 
-        final Greetings greeting = Greetings.builder()
-                                      .message(message)
-                                      .description(description)
-                                      .timestamp(timestamp)
-                                      .build();
+        final Greetings greeting = Greetings.builder().message(message).description(description).timestamp(timestamp).build();
 
         final Greetings unMutatedGreeting = SerializationUtils.clone(greeting);
 
