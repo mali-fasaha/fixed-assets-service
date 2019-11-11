0f0c64d3d41167002bb9fc30e24e110e4110bce1
Edwin Njeru <mailnjeru@gmail.com>
2019-11-11 11:37:17 +0300

    Implement asset-depreciation-mto

--- src/main/java/io/github/assets/app/messaging/assetDepreciation/AssetDepreciationMTO.java
+++ src/main/java/io/github/assets/app/messaging/assetDepreciation/AssetDepreciationMTO.java
@@ -7,6 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -17,4 +21,9 @@ public class AssetDepreciationMTO implements TokenizableMessage<String> {
     private String description;
     private long timestamp;
     private String messageToken;
+    private Long id;
+    private double depreciationAmount;
+    private String depreciationDate;
+    private Long categoryId;
+    private Long assetItemId;
 }
